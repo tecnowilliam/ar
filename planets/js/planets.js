244c6b1a64a7d153f2040380c49d3aece7f8a42a
William Vargas <wvargas@alertlogic.com>
2020-02-15 18:30:14 -0500

    Finish venus and earth

--- planets/js/planets.js
+++ planets/js/planets.js
@@ -1,4 +1,5 @@
-var scene,
+var planetImg,
+	scene,
     ambientLight,
     camera,
     renderer,
@@ -8,20 +9,28 @@ var scene,
     arToolkitSource,
     arToolkitContext,
     markerRoot1,
-    mesh1;
+	mesh1,
+	rotation = 0;
 
 window.onload = function()
 {
-    console.log("planets: 0.0.1");
+	console.log('planets: 0.1.0');
+	getParams();
     initialize();
     animate();
+}
+
+function getParams()
+{
+	const urlParams = new URLSearchParams(window.location.search);
+	planetImg 		= 'images/'+urlParams.get('planet')+'.jpg';
 }
 
 function initialize()
 {
 	scene        = new THREE.Scene();
-	ambientLight = new THREE.AmbientLight( 0xcccccc, 0.5 );
-    scene.add( ambientLight );
+	ambientLight = new THREE.AmbientLight();
+    scene.add(ambientLight);
 
 	camera = new THREE.Camera();
 	scene.add(camera);
@@ -31,13 +40,11 @@ function initialize()
 		alpha: true
     });
 
-	renderer.setClearColor(new THREE.Color('lightgrey'), 0)
-    // renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setSize( 640, 480 );
-	renderer.domElement.style.position  = 'absolute'
-	renderer.domElement.style.top       = '0px'
-	renderer.domElement.style.left      = '0px'
-	document.body.appendChild( renderer.domElement );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.domElement.style.position = 'absolute'
+	renderer.domElement.style.top      = '0px'
+	renderer.domElement.style.left     = '0px'
+	document.getElementById('div-cam').appendChild(renderer.domElement);
 
 	clock     = new THREE.Clock();
 	deltaTime = 0;
@@ -55,10 +62,8 @@ function initialize()
 	{
 		arToolkitSource.onResize()
 		arToolkitSource.copySizeTo(renderer.domElement)
-		if ( arToolkitContext.arController !== null )
-		{
+		if (arToolkitContext.arController !== null)
 			arToolkitSource.copySizeTo(arToolkitContext.arController.canvas)
-		}
 	}
 
 	arToolkitSource.init(function onReady(){
@@ -94,39 +99,24 @@ function initialize()
     scene.add(markerRoot1);
 
 	let markerControls1 = new THREEx.ArMarkerControls(arToolkitContext, markerRoot1, {
-		type: 'pattern', patternUrl: "data/hiro.patt",
+		type: 'pattern', patternUrl: 'data/hiro.patt',
     })
-
-    console.log(render);
-    console.log(markerControls1);
 
 	let geometry1   = new THREE.SphereGeometry(1,32,32);
 	let loader      = new THREE.TextureLoader();
-	let texture     = loader.load(planet, render);
+	let texture     = loader.load(planetImg, render);
     let material1   = new THREE.MeshLambertMaterial({map:texture, opacity:1});
 
 	mesh1            = new THREE.Mesh(geometry1, material1);
 	mesh1.position.y = 1;
 
 	markerRoot1.add(mesh1);
-
-	// let pointLight = new THREE.PointLight(0xffffff, 1, 100);
-    // pointLight.position.set(0.5,3,2);
-
-	// // create a mesh to help visualize the position of the light
-	// pointLight.add(
-	// 	new THREE.Mesh(
-	// 		new THREE.SphereBufferGeometry(0.05, 16,8),
-	// 		new THREE.MeshBasicMaterial({color:0xffffff, opacity:1})
-	// 	)
-    // );
-	// markerRoot1.add(pointLight);
 }
 
 function update()
 {
-	// if (markerRoot1.visible)
-    //     mesh1.rotation.y += 0.01;
+	if (markerRoot1.visible)
+        mesh1.rotation.y += rotation;
 
 	// update artoolkit on every frame
 	if (arToolkitSource.ready !== false)
@@ -145,4 +135,16 @@ function animate()
 	totalTime += deltaTime;
 	update();
 	render();
+}
+
+function action(action)
+{
+	if (action == 'menu')
+		window.location.href = 'index.html';
+
+	if (action == 'move')
+		rotation = 0.01
+
+	if (action == 'stop')
+		rotation = 0
 }
